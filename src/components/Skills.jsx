e881ce58cf215377feac4bc1ee6c6cd11c77a642
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-02-19 17:33:21 -0300

    About modificacao

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -73,94 +73,42 @@ function Skills() {
       <SkillsBoxStyled>
         <SkillStyled>
           <a
-            href="https://www.gnu.org/software/bash/"
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
             target="_blank"
             rel="noreferrer"
           >
             <img
-              src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg"
-              alt="bash"
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+              alt="javascript"
             />
           </a>
-          <h3>Bash</h3>
+          <h3>JavaScript</h3>
         </SkillStyled>
         <SkillStyled>
           <a
-            href="https://getbootstrap.com"
+            href="https://www.typescriptlang.org/"
             target="_blank"
             rel="noreferrer"
           >
             <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
-              alt="bootstrap"
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
+              alt="typescript"
             />
           </a>
-          <h3>Bootstrap</h3>
+          <h3>TypeScript</h3>
         </SkillStyled>
         <SkillStyled>
           <a
-            href="https://www.w3schools.com/css/"
+            href="https://www.python.org"
             target="_blank"
             rel="noreferrer"
           >
             <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
-              alt="css3"
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+              alt="python"
             />
           </a>
-          <h3>Css</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://www.docker.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg"
-              alt="docker"
-            />
-          </a>
-          <h3>Docker</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://expressjs.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
-              alt="express"
-            />
-          </a>
-          <h3>Express</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://git-scm.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
-              alt="git"
-            />
-          </a>
-          <h3>Git</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://heroku.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg"
-              alt="heroku"
-            />
-          </a>
-          <h3>Heroku</h3>
+          <h3>Python</h3>
         </SkillStyled>
         <SkillStyled>
           <a
@@ -177,146 +125,29 @@ function Skills() {
         </SkillStyled>
         <SkillStyled>
           <a
-            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            href="https://www.w3schools.com/css/"
             target="_blank"
             rel="noreferrer"
           >
             <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-              alt="javascript"
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+              alt="css3"
             />
           </a>
-          <h3>JavaScript</h3>
+          <h3>Css</h3>
         </SkillStyled>
         <SkillStyled>
           <a
-            href="https://jestjs.io"
+            href="https://getbootstrap.com"
             target="_blank"
             rel="noreferrer"
           >
             <img
-              src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg"
-              alt="jest"
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
+              alt="bootstrap"
             />
           </a>
-          <h3>Jest</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://www.linux.org/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg"
-              alt="linux"
-            />
-          </a>
-          <h3>Linux</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://mochajs.org"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://www.vectorlogo.zone/logos/mochajs/mochajs-icon.svg"
-              alt="mocha"
-            />
-          </a>
-          <h3>Mocha</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://www.mongodb.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
-              alt="mongodb"
-            />
-          </a>
-          <h3>MongoDb</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://www.mysql.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
-              alt="mysql"
-            />
-          </a>
-          <h3>Mysql</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://www.nginx.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg"
-              alt="nginx"
-            />
-          </a>
-          <h3>Nginx</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://nodejs.org"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
-              alt="nodejs"
-            />
-          </a>
-          <h3>Node Js</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://www.postgresql.org"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
-              alt="postgresql"
-            />
-          </a>
-          <h3>Postgresql</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://postman.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
-              alt="postman"
-            />
-          </a>
-          <h3>Postman</h3>
-        </SkillStyled>
-        <SkillStyled>
-          <a
-            href="https://www.python.org"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
-              alt="python"
-            />
-          </a>
-          <h3>Python</h3>
+          <h3>Bootstrap</h3>
         </SkillStyled>
         <SkillStyled>
           <a
@@ -346,16 +177,42 @@ function Skills() {
         </SkillStyled>
         <SkillStyled>
           <a
-            href="https://www.typescriptlang.org/"
+            href="https://nodejs.org"
             target="_blank"
             rel="noreferrer"
           >
             <img
-              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
-              alt="typescript"
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+              alt="nodejs"
             />
           </a>
-          <h3>TypeScript</h3>
+          <h3>Node Js</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://expressjs.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+              alt="express"
+            />
+          </a>
+          <h3>Express</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://www.nginx.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg"
+              alt="nginx"
+            />
+          </a>
+          <h3>Nginx</h3>
         </SkillStyled>
         <SkillStyled>
           <a
@@ -369,6 +226,149 @@ function Skills() {
             />
           </a>
           <h3>Sequelize</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://www.mongodb.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+              alt="mongodb"
+            />
+          </a>
+          <h3>MongoDb</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://www.mysql.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+              alt="mysql"
+            />
+          </a>
+          <h3>Mysql</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://www.postgresql.org"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+              alt="postgresql"
+            />
+          </a>
+          <h3>Postgresql</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://www.gnu.org/software/bash/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg"
+              alt="bash"
+            />
+          </a>
+          <h3>Bash</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://www.docker.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg"
+              alt="docker"
+            />
+          </a>
+          <h3>Docker</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://heroku.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg"
+              alt="heroku"
+            />
+          </a>
+          <h3>Heroku</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://postman.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+              alt="postman"
+            />
+          </a>
+          <h3>Postman</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://mochajs.org"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://www.vectorlogo.zone/logos/mochajs/mochajs-icon.svg"
+              alt="mocha"
+            />
+          </a>
+          <h3>Mocha</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://jestjs.io"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg"
+              alt="jest"
+            />
+          </a>
+          <h3>Jest</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://www.linux.org/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg"
+              alt="linux"
+            />
+          </a>
+          <h3>Linux</h3>
+        </SkillStyled>
+        <SkillStyled>
+          <a
+            href="https://git-scm.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+              alt="git"
+            />
+          </a>
+          <h3>Git</h3>
         </SkillStyled>
       </SkillsBoxStyled>
     </SkillsStyle>
