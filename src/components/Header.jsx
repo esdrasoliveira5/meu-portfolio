02fbb84b6266cf43ab16b33072184c586d78f60f
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-02-21 19:08:16 -0300

    header responsivo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,54 +11,64 @@ function Header() {
   const animation = nightMode ? 'whiteToBlack' : 'BlackToWhite';
 
   const HeaderStyled = styled.header`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  width: 100%;
-  height: 100px;
-  z-index: 1;
-  background-color: ${backgroundColor};
-  color:${fontColor};
-
-  animation: ${animation} 3s;
-  @keyframes whiteToBlack {
-    from {background-color: white;}
-    to {background-color: #2B2D42;}
-  }
-  @keyframes BlackToWhite {
-    from {background-color: #2B2D42;}
-    to {background-color: white;}
-  }
-  a {
-    text-decoration: none;
-    color: ${fontColor};
-  }
+    padding: 10px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 100px;
+    z-index: 1;
+    background-color: ${backgroundColor};
+    color:${fontColor};
+    animation: ${animation} 3s;
+    nav {
+      @media screen and (max-width: 768px) {
+        font-size: 12px;
+      }
+      display: flex;
+      justify-content: space-around;
+      flex-grow: 6;
+    }
+    @keyframes whiteToBlack {
+      from {background-color: white;}
+      to {background-color: #2B2D42;}
+    }
+    @keyframes BlackToWhite {
+      from {background-color: #2B2D42;}
+      to {background-color: white;}
+    }
+    a {
+      text-decoration: none;
+      color: ${fontColor};
+    }
   `;
   return (
     <HeaderStyled>
       <h1>Portfólio</h1>
-      <Link
-        to="/"
-      >
-        Home
-      </Link>
-      <Link
-        to="/work"
-      >
-        Work
-      </Link>
-      <Link
-        to="/about"
-      >
-        About
-      </Link>
-      <Link
-        to="/contact"
-      >
-        Contact
-      </Link>
+      <nav>
+        <Link
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          to="/work"
+        >
+          Work
+        </Link>
+        <Link
+          to="/about"
+        >
+          About
+        </Link>
+        <Link
+          to="/contact"
+        >
+          Contact
+        </Link>
+      </nav>
       <Switch />
     </HeaderStyled>
   );
